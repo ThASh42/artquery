2e75ad2a2456f868f9c3afb131fd7d52925ae360
ThASh42 <thashmail42@gmail.com>
2024-06-11 11:00:34 +0300

    Create user account system

--- querygenerator/urls.py
+++ querygenerator/urls.py
@@ -5,4 +5,8 @@ app_name = 'querygenerator'
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('register/', views.user_register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- querygenerator/views.py
+++ querygenerator/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
-# Create your views here.
+
 def index(request):
     return render(request, "index.html")
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('querygenerator:index')
+    else:
+        form = CustomUserCreationForm()
+    
+    return render(request, 'register.html', {
+        'form': form,
+    })
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('querygenerator:index')
+    else:
+        form = CustomAuthenticationForm()
+    
+    return render(request, 'login.html', {
+        'form': form,
+    })
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('querygenerator:login')
