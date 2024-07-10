8ea1d081fbe4b553dcb63a84cc5a1ede4d6f3856
ThASh42 <thashmail42@gmail.com>
2024-07-10 15:13:19 +0300

    Initial register page front-end

--- querygenerator/forms.py
+++ querygenerator/forms.py
@@ -6,7 +6,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ('username', 'email',)
-
+    
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Repeat your password'})
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
