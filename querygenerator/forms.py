74888c434f9d2acc237316250ddf8d986585f398
ThASh42 <thashmail42@gmail.com>
2024-07-10 15:39:21 +0300

    Update form field design

--- querygenerator/forms.py
+++ querygenerator/forms.py
@@ -9,10 +9,11 @@ class CustomUserCreationForm(UserCreationForm):
     
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
-        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
-        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
-        self.fields['password2'].widget.attrs.update({'placeholder': 'Repeat your password'})
+        DEFAULT_ATTRS = {'class': 'form-control width-300'}
+        self.fields['username'].widget.attrs.update(DEFAULT_ATTRS | {'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update(DEFAULT_ATTRS | {'placeholder': 'Email'})
+        self.fields['password1'].widget.attrs.update(DEFAULT_ATTRS | {'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update(DEFAULT_ATTRS | {'placeholder': 'Repeat your password'})
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
