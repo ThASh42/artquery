aa8136fa95263e2093fd55bc7a0c0378a6723aff
ThASh42 <thashmail42@gmail.com>
2024-08-05 15:19:17 +0300

    Create core folder

--- core/artquery/settings.py
+++ core/artquery/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'artquery.urls'
+ROOT_URLCONF = 'core.artquery.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'artquery.wsgi.application'
+WSGI_APPLICATION = 'core.artquery.wsgi.application'
 
 
 # Database
