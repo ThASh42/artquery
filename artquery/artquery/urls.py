7dafc3d403e0ad6032f1e3cbc4b22a36983354f5
ThASh42 <thashmail42@gmail.com>
2024-10-11 15:41:39 +0300

    Copy authentication functionality from private repository

--- artquery/artquery/urls.py
+++ artquery/artquery/urls.py
@@ -1,13 +1,13 @@
-# from artquery.authentication.views.login import LoginView
 from django.contrib import admin
 from django.urls import include, path
 
 import artquery.accounts.urls
+from artquery.authentication.views.login import LoginView
 
 API_PREFIX = 'api/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('login', LoginView.as_view(), name='login'),
+    path('login', LoginView.as_view(), name='login'),
     path(API_PREFIX, include(artquery.accounts.urls)),
 ]
---
+++ artquery/authentication/views/login.py
@@ -0,0 +1,27 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from artquery.accounts.serializers.accounts import AccountSerializer
+
+from ..serializers.login import LoginSerializers
+
+
+class LoginView(APIView):
+
+    @staticmethod
+    def post(request):
+        serializer = LoginSerializers(
+            data=request.data, context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        account = serializer.save()
+        refresh_token = RefreshToken.for_user(account)
+
+        return Response({
+            'account': AccountSerializer(account).data,
+            'authentication': {
+                'access_token': str(refresh_token.access_token),
+                'refresh_token': str(refresh_token),
+            },
+        })
