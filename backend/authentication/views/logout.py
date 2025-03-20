64f5a317627f8c92f3a28272a12812efd2d39bfe
ThASh42 <thashmail42@gmail.com>
2025-03-20 12:26:52 +0200

    Implement JWT cookie deletion on logout

--- backend/authentication/views/logout.py
+++ backend/authentication/views/logout.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import logout
-from django.shortcuts import redirect
 from django.urls import reverse
 from django.views import View
-
+from django.http import HttpResponseRedirect
 
 class LogoutView(View):
 
     def post(self, request, *args, **kwargs):
         logout(request)
-        return redirect(reverse('users:register'))
+
+        response = HttpResponseRedirect(reverse('authentication:login'))
+        response.delete_cookie('jwt')
+
+        return response
