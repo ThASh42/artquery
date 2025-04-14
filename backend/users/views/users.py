ce2aa4c81b7df9dcd0e0855f3d5f331cc7dad19a
ThASh42 <thashmail42@gmail.com>
2025-04-14 18:27:48 +0300

    Implement CustomUserCreationForm for user registration

--- backend/users/views/users.py
+++ backend/users/views/users.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.views import View
 
 from ..api.v1.serializers.users import UserSerializer
+from ..forms.users import CustomUserCreationForm
 
 
 class UserRegisterView(View):
     serializer_class = UserSerializer
 
     def get(self, request):
-        serializer = self.serializer_class()
-        context = {"serializer": serializer}
+        register_form = CustomUserCreationForm()
+        context = {"register_form": register_form}
         return render(request, "accounts/register.html", context)
