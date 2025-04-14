049bd66ba8705810fa2594ee01eed73ff4cb6dbb
ThASh42 <thashmail42@gmail.com>
2025-04-14 18:50:31 +0300

    Update CustomAuthenticationForm

--- backend/authentication/forms/login.py
+++ backend/authentication/forms/login.py
@@ -4,45 +4,61 @@ from django.utils.translation import gettext_lazy as _
 
 from backend.users.models import CustomUser
 
-DEFAULT_ATTRS = {"class": "form-control width-300"}
+DEFAULT_ATTRS = {"class": "form-control"}
 
 
 class CustomAuthenticationForm(forms.Form):
 
     username = forms.CharField(
         label=_("Username or email address"),
+        max_length=264,
+        required=True,
+        strip=True,
         widget=forms.TextInput(
             attrs={
                 **DEFAULT_ATTRS,
-                "placeholder": _("Username or email address"),
-            }
+                "placeholder": _("Enter your username or email"),
+            },
         ),
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={**DEFAULT_ATTRS})
+        label=_("Password"),
+        required=True,
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                **DEFAULT_ATTRS,
+                "placeholder": "**********",
+            },
+        ),
     )
 
     class Meta:
         model = CustomUser
         fields = (
-            "email",
+            "username",
             "password",
         )
 
-    def __init__(self, *args, **kwargs):
-        super(CustomAuthenticationForm, self).__init__(*args, **kwargs)
-
     def clean(self):
-        email = self.cleaned_data.get("email")
+        username = self.cleaned_data.get("username")
         password = self.cleaned_data.get("password")
-        if email and password:
-            self.user_cache = authenticate(username=email, password=password)
+
+        if username and password:
+            self.user_cache = authenticate(
+                username=username, password=password
+            )
             if self.user_cache is None:
                 raise forms.ValidationError(
-                    "Invalid email or password.",
+                    _("Invalid email or password."),
                     code="invalid_login",
+                )
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(
+                    _("This account is inactive."),
+                    code="inactive",
                 )
         return self.cleaned_data
 
     def get_user(self):
-        return self.user_cache
+        return getattr(self, "user_cache", None)
